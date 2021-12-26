d45d3eb9ec91e9bcdabdef61505785cad6d46281
Andrew Gordon <andrew.w.gordon@btinternet.com>
2021-12-26 11:20:43 +0000

    Web forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
+from flask.helpers import flash, url_for
+from werkzeug.utils import redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +19,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home',user=user,posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login reqyested for user {}, rember me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
